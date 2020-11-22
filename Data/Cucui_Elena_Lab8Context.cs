473168c0031396ef47122fa645029c191c3b3a52
ELENA - MARIANA CUCUI <cucui.elena60109@student.ubbcluj.ro>
2020-11-22 10:43:11 +0200

    Add project files.

---
+++ Data/Cucui_Elena_Lab8Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Cucui_Elena_Lab8.Models;
+
+namespace Cucui_Elena_Lab8.Data
+{
+    public class Cucui_Elena_Lab8Context : DbContext
+    {
+        public Cucui_Elena_Lab8Context (DbContextOptions<Cucui_Elena_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Cucui_Elena_Lab8.Models.Book> Book { get; set; }
+    }
+}
---
+++ Pages/Books/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Cucui_Elena_Lab8.Data;
+using Cucui_Elena_Lab8.Models;
+
+namespace Cucui_Elena_Lab8.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Cucui_Elena_Lab8.Data.Cucui_Elena_Lab8Context _context;
+
+        public IndexModel(Cucui_Elena_Lab8.Data.Cucui_Elena_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Book = await _context.Book.ToListAsync();
+        }
+    }
+}
